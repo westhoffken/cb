a6ba330b174ea05b8f38f0039425724bf4f20e85
Kenneth Westhoff <kwesthoff@icit.nl>
2023-06-15 22:10:55 +0200

    WIP

--- src/Classes/Calculator.php
+++ src/Classes/Calculator.php
@@ -19,6 +19,7 @@ class Calculator
      * @var array
      */
     private array $commands;
+    private ExpressionLanguage $evaluater;
 
 
     /**
@@ -28,20 +29,83 @@ class Calculator
     public function __construct(string $sum, array $commands)
     {
         $this->sum = $sum;
-        $this->commands = $commands;
+//        $this->commands = $commands;
+        $this->commands = [
+            "sqrt(50.50*(sqrt(50 * sqrt(50))))",
+        ];
+        $this->evaluater = new ExpressionLanguage();
     }
 
 
     public function calculate()
     {
 
-        // CLean up gaat eerst door de array heen om te kijken of we niet beginnen en eindigen met operators
-        $this->cleanUp();
-        dump($this->commands);
-        // Parse all commands into a proper sum
-        $this->parseCommands();
 
-        return (new ExpressionLanguage())->evaluate($this->sum);
+        //TODO: recerusive function that solves any regex on teh deepest lever in a single command
+//
+        // Revursively call the command and check if there are opperators needed
+        // CLean up gaat eerst door de array heen om te kijken of we niet beginnen en eindigen met operators
+//        $this->cleanUp();
+
+        dump($this->commands);
+//        // Parse all commands into a proper sum
+        $this->parseCommands();
+        dump($this->commands);
+
+        return (new ExpressionLanguage())->evaluate('10 * (10 + 10*(100 * 100 - 20))');
+    }
+
+    private function simpleSum(int $key, string $command): void
+    {
+        // Wat hier de bedoeling is, is een methode die kijkt of er een simpele som aanwezig is en deze oplost
+        if (preg_match('/-?\d+(\.\d+)?\h*[-+*\/]\h*-?\d+(\.\d+)?/', $command, $sum)) {
+            dump($sum[0]);
+
+            // Vervang de waarde van de som waar we op dit moment mee bezig zijn en ga verder
+            $this->commands[$key] = str_replace($sum[0], $this->evaluater->evaluate($sum[0]), $this->commands[$key]);
+        }
+
+    }
+
+    private function squirt(int $key, string $command)
+    {
+        // TODO: almost there! the match below does not like it when we have a single quit with nothing
+        // so i need to figure that out
+        // This is fun and all but yolo xD
+        // SOmehow we need to keep track of the command we are working on and calculate stuff without sqrt
+        if (preg_match('/sqrt\(([^()]*)\)/', $command, $matches)) {
+
+
+            dump($matches);
+            preg_match('/\d+(,\d+)?/', $matches[0], $number);
+
+
+            // maar hoe ga ik nu het nummer vervangen en de som opnieuw laten draaien. dan moet het command aangepast zijn
+            $squirt = sqrt($number[0]);
+            dump('matched: ' . $matches[0][0]);
+            dump('calculate:' . $squirt);
+            // What we also need to do is remove ( and ) because it just became a sperate item
+            $this->commands[$key] = str_replace($matches[0], $squirt, $this->commands[$key]);
+            dump($this->commands);
+            $this->simpleSum($key, $this->commands[$key]);
+
+            // Het lijkt heel veel, maar ik wil graag in de som kijken op diepste nivuea of er nog ergens een ({getal}) stata
+            // wat tussenhaakjes staat en verder geen zaken erbij heeft. in dit geval kan ik de haakjes namelijk verwijderen
+            if (preg_match('/([*\/+-])\((\d+(\.\d+))\)/', $this->commands[$key], $cleanedUpSum)) {
+                dump($cleanedUpSum);
+                $this->commands[$key] = str_replace($cleanedUpSum[0], $cleanedUpSum[1] . $cleanedUpSum[2], $this->commands[$key]);
+            }
+            //Het ziet er misschien wat gek uit, maar bovenstaande regex rekent alle haakjes
+            // Als matches 1,0 gebruik voor het uitlezen van een srt(*) operation dan krijg ik een haakje teveel mee
+            // Om dat te voorkomen doe ik nog een extra filter en pas ik daar de som op toe
+            dump($this->commands);
+            $this->squirt($key, $this->commands[$key]);
+        }
+
+
+        // so there is no more single squirt, now lets find all parenteheses and solve those
+        die();
+
     }
 
     /**
@@ -50,7 +114,9 @@ class Calculator
     private function parseCommands(): void
     {
         foreach ($this->commands as $key => $command) {
-            $this->parseSqrt($key, $command);
+            $this->squirt($key, 'sqrt(50.50(sqrt(50)))');
+//            $this->addMissingOperatorsInCommands($key, $command);
+//            $this->parseSqrt($key, $command);
 
         }
         // implode maakt van een array een string en in dit geval een die ik kan gebruiken voor de som
@@ -66,7 +132,6 @@ class Calculator
     private function parseSqrt(int $key, string $item): void
     {
         // Omdat square root niet onderdeel uitmaakt van de evaluater die ik gebruik moet deze eerst omgezet worden naar een getal
-
 
         if (preg_match('/sqrt\((\d+(,\d+)?)\)/', $item, $matches)) {
             // We have a match, now we just want to find the number, so we can calculate it properly
@@ -108,6 +173,45 @@ class Calculator
             $newCommands[] = $command;
         }
         $this->commands = $newCommands;
+    }
+
+    /**
+     *
+     * @param int $key
+     * @param string $command
+     * @return void
+     */
+    private function addMissingOperatorsInCommands(int $key, string $command): void
+    {
+
+        // TODO: see if it concerns a sqrt, then call a method that recursively checks if it has () then call method again for squirt
+
+        preg_match_all('/\(([^()]*)\)/', $command, $matches);
+
+        // Het idee is om het diepste niveau te kijken naar eventuele sommen die geen operators hebben en deze automatisch toe te voegen
+        if (isset($matches[0])) {
+            $this->addMissingOperatorsInCommands($key, $command);
+        } else {
+            // Niet elk command in de reeks moet voorzien worden van een command, begint het met een '(" dan mag er gekeken worden!
+            // Dit hadden we ook kunnen oplossen met een substr, maar ik ben nu het hele document bezig met regex
+            preg_match_all('/\(/', $command, $matches);
+            if (isset($matches[0]) && OperatorUtils::needsOperatorBefore($key, $this->commands)) {
+
+                //operator in stack
+                $this->commands[$key] = '*' . $command;
+            }
+            preg_match_all('/\)/', $command, $matches);
+
+            if (isset($matches[0]) && OperatorUtils::needsOperatorAfter($key, $this->commands)) {
+
+                $this->commands[$key] .= '*';
+            }
+        }
+
+
+        dump($this->commands);
+
+        dump($matches);
     }
 
 
